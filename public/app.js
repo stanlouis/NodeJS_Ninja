33b38e4afd9fee32f308735d7a790803168d1ae4
Stanley Louis <stanlouis@msn.com>
2017-11-01 12:40:44 -0400

    Modules and Require
    Learn how to use module.exports and require.

--- public/app.js
+++ public/app.js
@@ -1,14 +1,3 @@
+const counter = require('./count');
 
-
-let time = 0;
-
-let timer = setInterval(() => {
-	time += 2;
-	console.log(`${time} seconds have passed`);
-	if (time >= 8) {
-		clearInterval(timer);
-	}
-}, 2000);
-
-console.log(__dirname);
-console.log(__filename);
+console.log(counter(['shaun', 'crystal', 'ryu']));
---
+++ public/count.js
@@ -0,0 +1,3 @@
+var counter = arr => `There are ${arr.length} elements in this array`;
+
+module.exports = counter;

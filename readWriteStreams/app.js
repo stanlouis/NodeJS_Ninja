19477508eaea8f384eb246cf103a2d579afda312
Stanley Louis <stanlouis@msn.com>
2017-11-02 14:15:19 -0400

    Read and Write Streams

--- readWriteStreams/app.js
+++ readWriteStreams/app.js
@@ -2,8 +2,9 @@ const http = require('http');
 const fs = require('fs');
 
 const myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+const myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
 
 myReadStream.on('data', (chunk) => {
 	console.log('new chunk received:');
-	console.log(chunk);
+	myWriteStream.write(chunk);
 });
